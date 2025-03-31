2c95c1ff682e043700eb2016377ac8b217956518
Duncan Moiyo <saretoduncan@gmail.com>
2025-03-31 16:25:18 +0300

    update dependent name from guardian to dependent

--- src/dto/patients.dto.ts
+++ src/dto/patients.dto.ts
@@ -1,39 +1,124 @@
-import { IsString, IsOptional, IsEmail, IsDateString } from "class-validator";
+import {
+  IsString,
+  IsOptional,
+  IsEmail,
+  IsDateString,
+  IsDate,
+} from 'class-validator';
 
 export class CreatePatientDto {
-    @IsString()
-    first_name: string;
-  
-    @IsOptional()
-    @IsString()
-    middle_name?: string;
-  
-    @IsString()
-    last_name: string;
-  
-    @IsOptional()
-    @IsEmail()
-    email?: string;
-  
-    @IsOptional()
-    @IsString()
-    phone_number?: string;
-  
-    @IsString()
-    residence: string;
-  
-    @IsDateString() // Ensures a valid date format (ISO 8601)
-    date_of_birth: string;
-  
-    @IsOptional()
-    @IsString()
-    guardian_name?: string;
-  
-    @IsOptional()
-    @IsString()
-    guardian_number?: string;
-  
-    @IsOptional()
-    @IsString()
-    guardian_relationship?: string;
-  }
+  @IsString()
+  first_name: string;
+
+  @IsOptional()
+  @IsString()
+  middle_name?: string;
+
+  @IsString()
+  last_name: string;
+
+  @IsString()
+  national_id_no: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsString()
+  phone_number: string;
+
+  @IsString()
+  residence: string;
+
+  @IsDate()
+  date_of_birth: Date;
+}
+
+export class UpdatePatientDto {
+  @IsOptional()
+  @IsString()
+  first_name?: string;
+
+  @IsOptional()
+  @IsString()
+  middle_name?: string;
+
+  @IsOptional()
+  @IsString()
+  last_name?: string;
+
+  @IsOptional()
+  @IsString()
+  national_id_no?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone_number?: string;
+
+  @IsOptional()
+  @IsString()
+  residence?: string;
+
+  @IsOptional()
+  @IsDate()
+  date_of_birth?: Date;
+}
+export class CreateDependentPatientDto {
+  @IsString()
+  first_name: string;
+
+  @IsOptional()
+  @IsString()
+  middle_name?: string;
+
+  @IsString()
+  last_name: string;
+
+  @IsDate()
+  date_of_birth: Date;
+
+  @IsOptional()
+  @IsString()
+  phone_number?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsString()
+  dependent_relationship: string;
+}
+
+export class UpdateDependentPatientDto {
+  @IsOptional()
+  @IsString()
+  first_name?: string;
+
+  @IsOptional()
+  @IsString()
+  middle_name?: string;
+
+  @IsOptional()
+  @IsString()
+  last_name?: string;
+
+  @IsOptional()
+  @IsDate()
+  date_of_birth?: Date;
+
+  @IsOptional()
+  @IsString()
+  phone_number?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  guardian_relationship?: string;
+}
