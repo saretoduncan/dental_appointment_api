a5180a46119708da9d1e09f35dfb852bb797081f
Duncan Moiyo <saretoduncan@gmail.com>
2025-04-03 18:05:15 +0300

    add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -12,8 +13,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  app.use(cookieParser())
+  app.use(cookieParser());
   dotenv.config();
+  const config = new DocumentBuilder()
+    .setTitle('DentistAppointmentApi')
+    .setDescription('Api used in managing and setting patients appoinments')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
